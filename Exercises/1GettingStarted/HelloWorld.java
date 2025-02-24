8af8eaa26d8cd8e37e1ae4ebf580fa168177df19
bhawana137 <bhabanas219@gmail.com>
2025-02-24 23:16:20 +0545

    Add files via upload

--- Exercises/1GettingStarted/HelloWorld.java
+++ Exercises/1GettingStarted/HelloWorld.java
@@ -1,11 +1,10 @@
-//Create a new project called “HelloWorld” and write a simple Java program that displays the message “Hello World” to the console.
+/* Create a new project called “HelloWorld” and write a simple Java program that
+  displays the message “Hello World” to the console. */
+
 package weekone;
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
 	}
-
 }
