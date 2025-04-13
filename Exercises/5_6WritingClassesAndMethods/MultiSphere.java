f47dce4032b66bdaffc1355c045d071b4c14794b
bhawana137 <bhabanas219@gmail.com>
2025-04-14 01:25:52 +0545

    Update MultiSphere.java

--- Exercises/5_6WritingClassesAndMethods/MultiSphere.java
+++ Exercises/5_6WritingClassesAndMethods/MultiSphere.java
@@ -1,3 +1,13 @@
+/*
+Sphere
+Design and implement a class called Sphere that contains instance data that represent a sphere’s diameter.
+Define a Sphere constructor to accept and initialize the diameter.
+Include getter and setter methods for the diameter.
+Include methods to calculate and return the volume and surface area of the sphere.
+Include a toString method that returns a one-line description of the sphere.
+Create a driver class (main class) called MultiSphere, whose main method instantiates and updates several Sphere objects.
+*/
+
 package week5;
 public class MultiSphere {
     public static void main(String[] args) {
