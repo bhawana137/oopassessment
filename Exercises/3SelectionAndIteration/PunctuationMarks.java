475d34c4f47061fec394e783ab58d0b33591a47c
bhawana137 <bhabanas219@gmail.com>
2025-04-14 01:05:45 +0545

    Update PunctuationMarks.java

--- Exercises/3SelectionAndIteration/PunctuationMarks.java
+++ Exercises/3SelectionAndIteration/PunctuationMarks.java
@@ -1,5 +1,11 @@
-package weekthree;
+/*
+Punctuation Marks
+Design and implement a program that counts the number of punctuation marks in the following string “Mary had a little lamb, her fleece was as 
+white as snow, and everywhere Mary went, the lamb was sure to go. -that was a nice poem- the end. ”. Produce a table that shows how many times each
+symbol occurred.
+*/
 
+package weekthree;
 public class PunctuationMarks {
     public static void main(String[] args) {
         // The input string
