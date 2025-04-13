8977778dcf1c96c7004637aca1011728ef9806ae
bhawana137 <bhabanas219@gmail.com>
2025-04-14 00:28:43 +0545

    Update and rename ans.md to Test.java

--- Exercises/1GettingStarted/Test.java
+++ Exercises/1GettingStarted/Test.java
@@ -174,14 +174,5 @@ Java uses curly braces {} to define code blocks. Removing the last } makes the c
 
 
 
-5 -
-a) As of February 2025, the latest release of the Java Standard Edition (Java SE) Platform is JDK 23.0.2. Additionally, JDK 21 is the most recent Long-Term Support (LTS) release. 
 
-b)  Java SE (Standard Edition) is used for making desktop and server applications. It includes core Java features like data structures, networking, and the Java Virtual Machine (JVM). In contrast, Java ME (Micro Edition) is for small devices like mobile phones, embedded systems, and IoT devices. It has a smaller set of features compared to Java SE.
-
-c) Java works on Windows, macOS, Linux, and even some mobile and embedded operating systems. Since Java is platform-independent, it can run on any OS that has a Java Virtual Machine (JVM).
-
-d) Apart from Eclipse, the most popular Java IDE is IntelliJ IDEA.
-
-e) The main() function is the starting point of any Java program. 
 
