a8f15f6c16f7a132abd9e205aef9ee7f36966abc
bhawana137 <bhabanas219@gmail.com>
2025-04-14 01:24:18 +0545

    Update CardGame.java

--- Exercises/5_6WritingClassesAndMethods/CardGame.java
+++ Exercises/5_6WritingClassesAndMethods/CardGame.java
@@ -1,3 +1,8 @@
+/*
+Design and implement a class called Card that represents a standard playing card. Each card has a suit and a face value. Create a program that
+deals five random cards.
+*/
+
 package week5;
 import java.util.ArrayList;
 import java.util.Collections;
