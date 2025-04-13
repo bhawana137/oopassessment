4fd3792b2d2df426f2ba24de8e8f0f64f0ca6b34
bhawana137 <bhabanas219@gmail.com>
2025-04-14 01:33:55 +0545

    Update PostalCodeApp.java

--- Exercises/9FurtherArrays/PostalCodeApp.java
+++ Exercises/9FurtherArrays/PostalCodeApp.java
@@ -1,3 +1,10 @@
+/*
+Post Codes
+Design and implement and application that reads a sequence of up to 25 pairs of names and postal codes for individuals. Store the data in an
+object designed to store a first name, last name and postal code(all as strings). Assume that each line of input will contain three strings, each
+separated by a tab character. After the input has been read, print the list in an appropriate format to the screen.
+*/
+
 package week9;
 import java.util.Scanner;
 
