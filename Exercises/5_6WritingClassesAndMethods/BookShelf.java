248f6025ac36b7add2527898cc264d7813a2e351
bhawana137 <bhabanas219@gmail.com>
2025-04-14 01:23:24 +0545

    Update BookShelf.java

--- Exercises/5_6WritingClassesAndMethods/BookShelf.java
+++ Exercises/5_6WritingClassesAndMethods/BookShelf.java
@@ -1,3 +1,12 @@
+/*
+Books
+Design and implement a class called Book that contains instance data for title, author, publisher and copyright date.
+Define that Book constructor to accept and initialize these data.
+Include setter and getter methods for all instance data.
+Include a toString method that returns a nicely formatted, multiline description of the book.
+Create a driver class called Bookshelf, whose main method instantiates and updates several Book objects.
+*/
+
 package week5;
 class Book {
     // Instance variables (attributes) to store information about the book
