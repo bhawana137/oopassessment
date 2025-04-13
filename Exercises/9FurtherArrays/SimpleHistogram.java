38575ca92248151a076db87f859090a27c69bb87
bhawana137 <bhabanas219@gmail.com>
2025-04-14 01:35:51 +0545

    Update SimpleHistogram.java

--- Exercises/9FurtherArrays/SimpleHistogram.java
+++ Exercises/9FurtherArrays/SimpleHistogram.java
@@ -1,3 +1,19 @@
+/*
+ Histogram
+Design and implement an application that creates a histogram that allows you to visually inspect the frequency distribution of a set of values. 
+The program should read in an arbitrary number of integers that are in the range 1 to 100 inclusive; then it should produce a chart similar to the 
+following one that indicates how many input values fell in the range 1 – 10, 11 – 20, and so on. Print one asterisk for each value entered.
+1 - 10  | ***
+11- 20  | *
+21- 30  | *****
+31- 40  |
+41- 50  | ******
+51 -60  | ***
+61- 70  | *
+71- 80  | *****
+81- 90  |
+91 -100 | **
+*/
 package week9;
 import java.util.Scanner;
 
