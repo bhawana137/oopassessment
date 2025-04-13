9fbd6cdd8a01504e42e6db6329419f83fe03461e
bhawana137 <bhabanas219@gmail.com>
2025-04-14 00:39:21 +0545

    Update Converter.java

--- Exercises/2DataAndExpressions/Converter.java
+++ Exercises/2DataAndExpressions/Converter.java
@@ -1,50 +1,5 @@
 /*
-Computing Averages The following program reads three integers and prints the average. Fill in the blanks so that it will work correctly.
-// *******************************************************************
-//   Average.java
-//
-//   Read three integers from the user and print their average
-// *******************************************************************
-
-
-import java.util.Scanner;
-public class Average
-{
-    public static void main(String[] args)
-    {
-       int val1, val2, val3;
-       double average;
-       Scanner scan = new Scanner(System.in) ;
-
-
-       // get three values from user
-       System.out.println("Please enter three integers and " +
-                       "I will compute their average");
-
-
-       ____________________________________
-
-
-       ____________________________________
-
-
-       ____________________________________
-
-
-       //compute the average
-
-
-       ____________________________________
-
-
-       //print the average
-
-
-       ____________________________________
-
-
-    }
-}
+Write an application that converts miles to kilometers.(One mile equals 1.60935 km). Read the miles from the user as a floating point value.
 */
 package week2;
 import java.util.Scanner;
